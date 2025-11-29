ad03597f4c90064a623e7cac7e30ada1449a1245
Your Name <you@example.com>
2025-11-29 23:54:54 +0400

    salam

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""salam"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """salam"""
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
